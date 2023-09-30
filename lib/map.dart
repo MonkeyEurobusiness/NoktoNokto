75b2e020f50d001861e508fdd5acc272376e90bf
demono10000 <48636182+demono10000@users.noreply.github.com>
2023-09-30 16:29:28 +0200

    added markers

--- lib/map.dart
+++ lib/map.dart
@@ -1,20 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class MapWidget extends StatelessWidget {
+class MapWidget extends StatefulWidget {
   final LatLng initialPosition;
 
   MapWidget({required this.initialPosition});
 
   @override
+  _MapWidgetState createState() => _MapWidgetState();
+}
+
+class _MapWidgetState extends State<MapWidget> {
+  Set<Marker> _markers = {};
+
+  void _onMapLongPress(LatLng position) {
+    final marker = Marker(
+      markerId: MarkerId(position.toString()),
+      position: position,
+    );
+
+    setState(() {
+      _markers.add(marker);
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return GoogleMap(
       initialCameraPosition: CameraPosition(
-        target: initialPosition,
+        target: widget.initialPosition,
         zoom: 14,
       ),
       myLocationEnabled: true,
       myLocationButtonEnabled: true,
+      onLongPress: _onMapLongPress,
+      markers: _markers,
     );
   }
 }

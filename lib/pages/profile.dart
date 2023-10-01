df83e14ac9eecc38f90cfe2cf28b2d00cdc60803
Ryszard Błażej <sapermysza@gmail.com>
2023-10-01 02:39:22 +0200

    Added support for additional pin graphics

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -18,20 +18,20 @@ class _ProfileWidgetState extends State<ProfileWidget> {
       body: Center(
         child: Column(
           children: [
-              Padding(padding: EdgeInsets.all(32)),
+              const Padding(padding: EdgeInsets.all(32)),
               Text("Hi username!", style: Theme.of(context).textTheme.displaySmall),
-              Padding(padding: EdgeInsets.all(16)),
+              const Padding(padding: EdgeInsets.all(16)),
               Row(
                 children: [
-                  Spacer(),
+                  const Spacer(),
                   IconButton(onPressed: (){setState(() {
                     _currentAvatar == 0 ? _currentAvatar = 6 : _currentAvatar -= 1;
-                  });}, icon: Icon(Icons.arrow_back_rounded, size: 40,)),
-                  CircleAvatar(child: Image.asset(avatarAssets[_currentAvatar], width: 120), radius: 100,),
+                  });}, icon: const Icon(Icons.arrow_back_rounded, size: 40,)),
+                  CircleAvatar(radius: 100,child: Image.asset(avatarAssets[_currentAvatar], width: 120),),
                   IconButton(onPressed: (){setState(() {
                     _currentAvatar == 6 ? _currentAvatar = 0 : _currentAvatar += 1;
-                  });}, icon: Icon(Icons.arrow_forward_rounded, size: 40,)),
-                  Spacer()
+                  });}, icon: const Icon(Icons.arrow_forward_rounded, size: 40,)),
+                  const Spacer()
                 ],
               ),
           ],
